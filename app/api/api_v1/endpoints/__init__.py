82206d4a92e46aac837cae248aef5ac59de15541
Arthi <90525298+ArthiDa@users.noreply.github.com>
2024-12-01 22:08:50 +0600

    Update __init__.py

--- app/api/api_v1/endpoints/__init__.py
+++ app/api/api_v1/endpoints/__init__.py
@@ -11,4 +11,5 @@ from .assignment import router as assignment_router
 
 from .student_event import router as student_event_router
 from .announcement import router as announcement_router
-from .student import router as student_routerfrom .clinical_practice import router as clinical_practice_router
+from .student import router as student_router
+from .clinical_practice import router as clinical_practice_router
